699ced32225d86d56a193a0f133bcbe4ba070a44
John Hamm <johndhamm1@gmail.com>
2017-01-03 16:26:55 -0600

    initial angular setup

---
+++ client/app/controllers/MainCtrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+app.controller("MainCtrl", function($scope, $http){
+
+	console.log("hello with mongoose");
+	$scope.message = "hello";
+
+
+})
--- server/server.js
+++ server/server.js
@@ -9,12 +9,24 @@ const { connect } = require('../db/database');
 const PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 
-// app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
+
+
+
+//middlewares
+app.use(express.static('client'));
+// app.use(json())
+
+
+
+
+// app.listen(PORT, () => {
+// 	console.log(`Express server listening on port ${PORT}`)
+// })
 
 connect()
 	.then(() => {
 		app.listen(PORT, () => {
-			console.log(`Express server listening on port: ${PORT}`);
+			console.log(`Mongoose server listening on port: ${PORT}`);
 		});
 	})
 	.catch(console.error)
